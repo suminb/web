cf2c46365f7201ba3e84a8a8e6ac913e65c61f4f
Sumin Byeon <suminb@gmail.com>
2011-01-31 14:14:07 -0700

    Reorganizing files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '488fa7b9c04f82786faef7110a22d0a83fd5a9b9d228cd6cba4541098aa9faf0630ac4d50faf16662bb66f6ff0602a0a9e6d10c87f4b2e7333f4148b8ca2cc43'
