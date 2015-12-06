db42dd94951b4fd2e1637849129ff6b30cef1e5a
Sumin Byeon <suminb@gmail.com>
2015-12-07 01:36:06 +0900

    Use Flask blueprint

--- web/__init__.py
+++ web/__init__.py
@@ -1,13 +1,13 @@
 import os
 
 from flask import Flask
-from flask import render_template
 from jinja2 import evalcontextfilter, Markup, escape
 from jinja2.environment import Environment
 try:
     from urllib import quote
 except:
     pass
+
 
 #
 # Custom filters for Jinja
@@ -21,6 +21,7 @@ def format_tags(eval_ctx, value, attr=''):
     else:
         return Markup(f(value))
 
+
 @evalcontextfilter
 def optional_url(eval_ctx, name, url):
     if url != None and len(url) > 0:
@@ -28,96 +29,17 @@ def optional_url(eval_ctx, name, url):
     else:
         return name
 
-app = Flask(__name__)
-app.jinja_env.filters['format_tags'] = format_tags
-app.jinja_env.filters['optional_url'] = optional_url
 
-#
-# Request handlers
-#
-@app.route("/")
-def index():
-    return render_template("index.html")
+def create_app(name=__name__, config={}, static_folder='static',
+               template_folder='templates'):
+    app = Flask(name)
 
-@app.route("/byeonbread")
-def byeonbread():
-    return render_template("byeonbread.html")
+    from web.main import main_module
+    from web.api import api_module
+    app.register_blueprint(main_module, url_prefix='')
+    app.register_blueprint(api_module, url_prefix='/api')
 
-@app.route("/history")
-def history():
-    return render_template("history.html")
+    app.jinja_env.filters['format_tags'] = format_tags
+    app.jinja_env.filters['optional_url'] = optional_url
 
-@app.route("/tagcloud")
-def tagcloud():
-    tags = extract_tags()
-    cloud = map(lambda t: '{text: "%s", weight: %d, link: "/tag/%s"}' % (t, tags[t], t), tags)
-    cloud_js = ",\n".join(cloud)
-
-    return render_template("tagcloud.html", cloud=Markup(cloud_js))
-
-
-@app.route("/projects")
-@app.route("/tag/<tag>")
-def projects(tag=None):
-    from pymongo import MongoClient
-    import re
-
-    connection = MongoClient(os.environ['DB_URL'], int(os.environ['DB_PORT']))
-    db = connection.resume
-    db.authenticate(os.environ['DB_USERNAME'], os.environ['DB_PASSWORD'])
-
-    query = None
-    if tag is None:
-        # http://docs.mongodb.org/manual/reference/operator/or/#_S_or
-        if re.match(r'\d+', tag):
-            print('year')
-            query = {'year': int(tag)}
-        else:
-            print('keyword')
-            query = {'$or': [{'keywords':tag}, {'languages':tag}]}
-
-    projects = db.projects.find(query)
-
-    return render_template("projects.html", projects=projects)
-
-
-def extract_tags():
-    import json 
-
-    def register_tag(tag_collection, tag):
-        if tag not in tag_collection:
-            tag_collection[tag] = 1
-        else:
-            tag_collection[tag] += 1
-
-    tags = {}
-    with open('projects.json') as f:
-        data = json.loads(f.read())
-
-        for row in data:
-            for kw in row['keywords']:
-                register_tag(tags, kw)
-            for ln in row['languages']:
-                register_tag(tags, ln)
-
-            if isinstance(row['year'], list):
-                for y in row['year']:
-                    register_tag(tags, y)
-            else:
-                register_tag(tags, row['year'])
-
-    return tags
-
-if __name__ == "__main__":
-    host = os.environ.get("HOST", "0.0.0.0")
-    port = int(os.environ.get("PORT", 8024))
-    debug = bool(os.environ.get("DEBUG", 0))
-
-    app.run(host=host, port=port, debug=debug)
-
-if app.config['DEBUG']:
-    from werkzeug import SharedDataMiddleware
-    import os
-    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-      '/': os.path.join(os.path.dirname(__file__), 'static')
-    })
+    return app
