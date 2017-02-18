92e512e14734afbb22a6da1bb0d41d1778b88678
Sumin Byeon <suminb@gmail.com>
2017-02-19 01:40:59 +0900

    Looks like the `debug` parameter has no effect

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8024))
     debug = bool(os.environ.get('DEBUG', 0))
 
-    app = create_app()
-    app.run(host=host, port=port, debug=debug)
+    app = create_app(config={'DEBUG': debug})
+    app.run(host=host, port=port)
 
     if app.config['DEBUG']:
         from werkzeug import SharedDataMiddleware
