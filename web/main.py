993ff8b5c3dc7c9cc66cb663924a02f2b7597018
Sumin Byeon <suminb@gmail.com>
2017-02-19 02:11:00 +0900

    Pass current page as a parameter

--- web/main.py
+++ web/main.py
@@ -6,4 +6,11 @@ main_module = Blueprint('main', __name__, template_folder='templates/main')
 
 @main_module.route('/')
 def index():
-    return render_template('index.html')
+    # FIXME: Could we automate this part somehow? (current_page)
+    return render_template('index.html', **{'current_page': 'index'})
+
+
+@main_module.route('/coding-expedition')
+def coding_expedition():
+    context = {'current_page': 'coding_expedition'}
+    return render_template('coding-expedition.html', **context)
